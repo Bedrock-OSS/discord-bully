8cc073f9f68aef1f2375b486cf33bd21cd7c7a8e
stirante <brzozowski.s.piotr@gmail.com>
2021-10-01 19:31:06 +0200

    Add more inputs

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const { Webhook, MessageBuilder} = require('discord-webhook-node');
 const axios = require('axios');
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 // The following code is a messy piece of code I did while being half asleep. Sorry.
 
-const WEBHOOK_URL = core.getInput('webhook');
+const EMOJI = core.getInput('emoji');
+const USER_MAP = JSON.parse(core.getInput('usermap'));
+const HOOK = new Webhook(core.getInput('webhook'));
 
-const hook = new Webhook(WEBHOOK_URL);
-
-const EMOJI_BONK = core.getInput('emoji');
-
-const USER_MAP = {
-  'stirante': '<@!210798002808422400>',
-  'SirLich': '<@!97792771619827712>',
-  'MedicalJewel105': '<@!738693702297321544>',
-  'sermah': '<@!305566858461970433>',
-  'SmokeyStack': '<@!530361907283099650>'
-}
-
-const COMMIT_URL = `https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA}`;
-const RUN_URL = `https://github.com/${process.env.GITHUB_REPOSITORY}/runs/${process.env.GITHUB_RUN_ID}`;
-const COMMIT_API = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/commits/${process.env.GITHUB_SHA}`;
+const COMMIT_URL = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/commit/${github.context.sha}`;
+const RUN_URL = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}`;
+const COMMIT_API = `https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/commits/${github.context.sha}`;
 
 async function sendBullyMessage() {
   let author = null;
@@ -33,18 +24,18 @@ async function sendBullyMessage() {
   }
 
   let mb = new MessageBuilder()
-    .setTitle("Wiki deployment failed")
+    .setTitle(core.getInput('title'))
     .setColor(0xeb4034)
     .addField("Build failed after commit", COMMIT_URL)
     .addField("Failed build", RUN_URL)
   if (author !== null) {
     if (USER_MAP[author] !== void 0) {
-      mb.setText("I blame " + USER_MAP[author] + " " + EMOJI_BONK)
+      mb.setText("I blame <@!" + USER_MAP[author] + "> " + EMOJI)
     } else {
-      mb.setText("I blame " + author + " " + EMOJI_BONK)
+      mb.setText("I blame " + author + " " + EMOJI)
     }
   }
-  await hook.send(mb);
+  await HOOK.send(mb);
 }
 
 sendBullyMessage();
