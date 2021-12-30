0d0d5e944b9ced4f1a5e81dbaf1b9255a1b5f7f2
stirante <brzozowski.s.piotr@gmail.com>
2021-12-30 21:37:28 +0100

    Add success after fail message as well

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # discord-bully
 
-Webhook based Discord bot that notifies Bedrock OSS members on discord of failed builds.
+Webhook based Discord bot that notifies Bedrock OSS members on discord of failed builds or succeeded builds after a failed one.
 
 Currently, the configuration is pretty much static, but full configuration is planned, so that others can use this action as well.
 
 ## Example configuration
 
 Add GitHub Action secrets `WEBHOOK_ID` and `WEBHOOK_TOKEN` from Discord webhook integrations.
+Add GitHub Action secret `TENOR_GIF_API_KEY` from Tenor GIF API.
 
 Add this step to GitHub Action:
 ```yaml
       - name: Notify of failed build
         if: ${{ failure() }}
-        uses: Bedrock-OSS/discord-bully@v1.0.3
+        uses: Bedrock-OSS/discord-bully@v1.0.4
         with:
           webhook: https://canary.discord.com/api/webhooks/${{ secrets.WEBHOOK_ID }}/${{ secrets.WEBHOOK_TOKEN }}
-          emoji: <:PE_PandaBonk:885395101877166110>
-          title: 'Wiki deployment failed!'
+          tenorGifApiKey: ${{ secrets.TENOR_GIF_API_KEY }}
+          failTitle: 'Wiki deployment failed!'
+          failMessage: 'I blame %author% <:PE_PandaBonk:885395101877166110>'
+          failGifQuery: 'fail'
+          failAuthor: 'You messed up'
+          successTitle: 'Wiki deployment succeeded!'
+          successMessage: 'Thank you, %author%! <:PE_PandaInLove:885395799029194853>'
+          gifSuccessQuery: 'success'
+          successAuthor: 'You did it'
+          failed: ${{ failure() }}
 ```
 
 Add to `index.js` a map of GitHub usernames to Discord IDs like this:
@@ -32,5 +41,5 @@ const USER_MAP = {
 
 ## TODO
 
-- [ ] Move user map outside of the code to allow easier configuration.
+- [ ] Move user map outside the code to allow easier configuration.
 - [ ] Move remaining message parts to the action config
--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const github = require('@actions/github');
 
 // The following code is a messy piece of code I did while being half asleep. Sorry.
 
-const MESSAGE = core.getInput('message');
+const FAIL_MESSAGE = core.getInput('failMessage');
+const SUCCESS_MESSAGE = core.getInput('successMessage');
+const FAIL_TITLE = core.getInput('failTitle');
+const SUCCESS_TITLE = core.getInput('successTitle');
+const FAIL_AUTHOR = core.getInput('failAuthor');
+const SUCCESS_AUTHOR = core.getInput('successAuthor');
+
+let FAILED = core.getInput('failed');
+
 const USER_MAP = {
     'stirante': '210798002808422400',
     'SirLich': '97792771619827712',
@@ -18,33 +26,90 @@ const HOOK = new Webhook(core.getInput('webhook'));
 const COMMIT_URL = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/commit/${github.context.sha}`;
 const RUN_URL = `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}`;
 const COMMIT_API = `https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/commits/${github.context.sha}`;
+const RUNS_URL = `https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs`;
 
-const TENOR_GIF_URL = `https://g.tenor.com/v1/random?q=${core.getInput('gifQuery')}&key=${core.getInput('tenorGifApiKey')}&limit=1&contentfilter=medium&media_filter=minimal`
+const TENOR_FAIL_GIF_URL = `https://g.tenor.com/v1/random?q=${core.getInput('gifFailQuery')}&key=${core.getInput('tenorGifApiKey')}&limit=1&contentfilter=medium&media_filter=minimal`
+const TENOR_SUCCESS_GIF_URL = `https://g.tenor.com/v1/random?q=${core.getInput('gifSuccessQuery')}&key=${core.getInput('tenorGifApiKey')}&limit=1&contentfilter=medium&media_filter=minimal`
 
-async function sendBullyMessage() {
+async function sendBullyOrNotMessage() {
+    // not really sure in what form I get the boolean value, so this is just a check
+    FAILED = FAILED === 'true' || FAILED === true || FAILED === 1;
     let author = null;
     let gif = '';
     try {
         let response = await axios.get(COMMIT_API)
         author = response.data.author.login;
-        response = await axios.get(TENOR_GIF_URL)
-        gif = response.data.results[0].media[0].gif.url
     } catch (e) {
         console.error('We actually failed to get the author');
         console.error(e);
     }
-
-    let mb = new MessageBuilder()
-        .setTitle(core.getInput('title'))
-        .setColor(0xeb4034)
-        .addField("Build failed after commit", COMMIT_URL)
-        .addField("Failed build", RUN_URL)
-        .setImage(gif)
-        .setText(MESSAGE
-            .replace('%author%', author !== null ? (USER_MAP[author] !== void 0 ? "<@!" + USER_MAP[author] + ">" : author) : 'unknown author')
-            .replace('%newline%', '\n')
-        )
-    await HOOK.send(mb);
+    if (FAILED) {
+        try {
+            let response = await axios.get(TENOR_FAIL_GIF_URL);
+            gif = response.data.results[0].media[0].gif.url
+        } catch (e) {
+            console.error('We failed to get a gif :(');
+            console.error(e);
+        }
+        let mb = new MessageBuilder()
+            .setAuthor(FAIL_AUTHOR)
+            .setTitle(FAIL_TITLE)
+            .setColor(0xeb4034)
+            .addField("Build failed after commit", COMMIT_URL)
+            .addField("Failed build", RUN_URL)
+            .setImage(gif)
+            .setText(FAIL_MESSAGE
+                .replace('%author%', author !== null ? (USER_MAP[author] !== void 0 ? "<@!" + USER_MAP[author] + ">" : author) : 'unknown author')
+                .replace('%newline%', '\n')
+            )
+        await HOOK.send(mb);
+    } else {
+        try {
+            // Get list of runs
+            let response = await axios.get(RUNS_URL)
+            let runs = response.data.workflow_runs;
+            // Find the run before
+            let found = false;
+            let previousRun = null;
+            for (const run of runs) {
+                if (found) {
+                    previousRun = run;
+                    break;
+                }
+                if (run.id === github.context.runId) {
+                    found = true;
+                }
+            }
+            if (previousRun === null) {
+                console.error('We failed to find the previous run. Using the first one instead.');
+                previousRun = runs[0];
+            }
+            // If the previous run is also successful, skip sending the message
+            if (previousRun.conclusion !== 'failure') {
+                return;
+            }
+        } catch (e) {
+            console.error('We failed to get previous runs');
+            console.error(e);
+        }
+        try {
+            let response = await axios.get(TENOR_SUCCESS_GIF_URL);
+            gif = response.data.results[0].media[0].gif.url
+        } catch (e) {
+            console.error('We failed to get a gif :(');
+            console.error(e);
+        }
+        let mb = new MessageBuilder()
+            .setAuthor(SUCCESS_AUTHOR)
+            .setTitle(SUCCESS_TITLE)
+            .setColor(0x32CD32)
+            .setImage(gif)
+            .setText(SUCCESS_MESSAGE
+                .replace('%author%', author !== null ? (USER_MAP[author] !== void 0 ? "<@!" + USER_MAP[author] + ">" : author) : 'unknown author')
+                .replace('%newline%', '\n')
+            )
+        await HOOK.send(mb);
+    }
 }
 
-sendBullyMessage();
+sendBullyOrNotMessage();
